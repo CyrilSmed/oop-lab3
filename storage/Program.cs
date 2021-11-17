8eb65da7e7eba9c9e221d53ec03babf5b69b7371
CyrilSmed <cyrilsmed@outlook.com>
2021-11-17 20:19:23 +0500

    Initialize Console Application

---
+++ storage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace storage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+
+}
+
